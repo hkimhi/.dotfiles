f55ec41233e0c296449fd3e19666e9414bbd3d59
Hod Kimhi <hodkimhi@gmail.com>
2025-02-12 03:36:27 -0800

    clean up

--- misc/playerctl.sh
+++ misc/playerctl.sh
@@ -2,32 +2,33 @@
 # inspiration: https://yarmo.eu/blog/playerctl-polybar/
 
 # title max length
-max_len=24
+max_len=16
 
 # separate font for play icon since it is a little larger
 font_icons=5
 font_play=6
 font=7
 
-playerctlstatus=$(playerctl status 2>/dev/null)
+status=$(playerctl status 2>/dev/null)
 
 
 title_format="{{ trunc(title, $max_len) }}"  # need this separate to pass in $max_len
-title="%{T$font}$(playerctl metadata --format "$title_format" )"
+title="%{T$font}$(playerctl metadata --format "$title_format" 2> /dev/null)"
 # title="${title/%…/...}" # replace ellipses from playerctl truncate with three periods
 
-prev="%{T$font_icons}%{A1:playerctl previous:}%{A}%{T-}"
-next="%{T$font_icons}%{A1:playerctl next:}%{A}%{T-}"
 progress="%{T$font}$(playerctl metadata --format '{{ duration(position) }} / {{ duration(mpris:length) }}' 2> /dev/null)"
 
-if [[ $playerctlstatus =~ "Playing" ]]; then
-	action="%{T$font_icons}%{A1:playerctl pause:}%{A}%{T-}"
+icon_prev="%{T$font_icons}%{A1:playerctl previous:}%{A}%{T-}"
+icon_next="%{T$font_icons}%{A1:playerctl next:}%{A}%{T-}"
+
+if [[ $status =~ "Playing" ]]; then
+	icon_action="%{T$font_icons}%{A1:playerctl pause:}%{A}%{T-}"
 else
-	action="%{T$font_play}%{A1:playerctl play:}%{A}%{T-}"
+	icon_action="%{T$font_play}%{A1:playerctl play:}%{A}%{T-}"
 fi
 
-format_str="$prev $iaction $inext $title ($progress)"
-if [[ $playerctlstatus == "" ]]; then
+format_str="$icon_prev $icon_action $icon_next $title ($progress)"
+if [[ $status == "" ]]; then
 	echo ""
 else
 	echo "%{F#FFFFFF}%{T$font} $format_str"
