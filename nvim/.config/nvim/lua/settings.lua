0f0f7fa2b15042bb22ae8f94d8ca4aacb5dc7765
Hod Kimhi <hodkimhi@gmail.com>
2025-07-04 19:08:45 -0700

    change vim.o for vim.opt to be more lua-esque

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -1,6 +1,5 @@
 local global = vim.g
-local o = vim.o
-local api = vim.api
+local opt = vim.opt
 
 global.mapleader = " "
 global.maplocalleader = " "
@@ -19,38 +18,38 @@ vim.cmd.colorscheme("fluoromachine")
 vim.opt.spell = false
 -- vim.opt.spelllang = { "en_ca" }
 
-o.number = true -- Print the line number in front of each line
-o.relativenumber = true -- Show the line number relative to the line with the cursor in front of each line.
-o.clipboard = "unnamedplus" -- uses the clipboard register for all operations except yank.
-o.syntax = "on" -- When this option is set, the syntax with this name is loaded.
-o.autoindent = true -- Copy indent from current line when starting a new line.
--- o.cursorline = true -- Highlight the screen line of the cursor with CursorLine.
--- o.expandtab = true -- In Insert mode: Use the appropriate number of spaces to insert a <Tab>.
-o.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent.
-o.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for.
-o.encoding = "utf-8" -- Sets the character encoding used inside Vim.
--- o.fileencoding = "utf-8" -- Sets the character encoding for the file of this buffer.
-o.ruler = true -- Show the line and column number of the cursor position, separated by a comma.
--- o.mouse = "a" -- Enable the use of the mouse. "a" you can use on all modes
-o.mouse = "" -- Disable the use of the mouse
-o.title = true -- When on, the title of the window will be set to the value of 'titlestring'
-o.hidden = true -- When on a buffer becomes hidden when it is |abandon|ed
-o.ttimeoutlen = 0 -- The time in milliseconds that is waited for a key code or mapped key sequence to complete.
-o.wildmenu = true -- When 'wildmenu' is on, command-line completion operates in an enhanced mode.
-o.showcmd = true -- Show (partial) command in the last line of the screen. Set this option off if your terminal is slow.
--- o.showmatch = true -- When a bracket is inserted, briefly jump to the matching one.
-o.inccommand = "split" -- When nonempty, shows the effects of :substitute, :smagic, :snomagic and user commands with the :command-preview flag as you type.
-o.splitbelow = true -- default behaviour of a horizontal split is below the current window
-o.splitright = true -- default behaviour of a vertical split is to the right of the current window
-o.termguicolors = true
-o.wrap = true
-o.linebreak = true
-o.background = 'light'
-vim.opt.completeopt = { "menuone", "noselect", "popup" }
+opt.number = true -- Print the line number in front of each line
+opt.relativenumber = true -- Show the line number relative to the line with the cursor in front of each line.
+opt.clipboard = "unnamedplus" -- uses the clipboard register for all operations except yank.
+opt.syntax = "on" -- When this option is set, the syntax with this name is loaded.
+opt.autoindent = true -- Copy indent from current line when starting a new line.
+-- opt.cursorline = true -- Highlight the screen line of the cursor with CursorLine.
+-- opt.expandtab = true -- In Insert mode: Use the appropriate number of spaces to insert a <Tab>.
+opt.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent.
+opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for.
+opt.encoding = "utf-8" -- Sets the character encoding used inside Vim.
+-- opt.fileencoding = "utf-8" -- Sets the character encoding for the file of this buffer.
+opt.ruler = true -- Show the line and column number of the cursor position, separated by a comma.
+-- opt.mouse = "a" -- Enable the use of the mouse. "a" you can use on all modes
+opt.mouse = "" -- Disable the use of the mouse
+opt.title = true -- When on, the title of the window will be set to the value of 'titlestring'
+opt.hidden = true -- When on a buffer becomes hidden when it is |abandon|ed
+opt.ttimeoutlen = 0 -- The time in milliseconds that is waited for a key code or mapped key sequence to complete.
+opt.wildmenu = true -- When 'wildmenu' is on, command-line completion operates in an enhanced mode.
+opt.showcmd = true -- Show (partial) command in the last line of the screen. Set this option off if your terminal is slow.
+-- opt.showmatch = true -- When a bracket is inserted, briefly jump to the matching one.
+opt.inccommand = "split" -- When nonempty, shows the effects of :substitute, :smagic, :snomagic and user commands with the :command-preview flag as you type.
+opt.splitbelow = true -- default behaviour of a horizontal split is below the current window
+opt.splitright = true -- default behaviour of a vertical split is to the right of the current window
+opt.termguicolors = true
+opt.wrap = false
+opt.linebreak = true
+opt.background = "light"
+opt.completeopt = { "menuone", "noselect", "popup" }
 
 vim.diagnostic.config({
 	virtual_text = false,
 	virtual_lines = false,
 })
 
-o.guicursor="n-v-c:block-Cursor,i-ci-ve:ver25-Cursor,r-cr:hor20,o:hor50"
+opt.guicursor = "n-v-c:block-Cursor,i-ci-ve:ver25-Cursor,r-cr:hor20,o:hor50"
