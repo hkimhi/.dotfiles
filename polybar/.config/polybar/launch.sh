3aea20994d3615916523b6b6f900e0f918f123ff
Hod Kimhi <hodkimhi@gmail.com>
2025-07-21 22:06:16 -0700

    launch polybar on all monitors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,5 +4,14 @@
 polybar-msg cmd quit
 # killall -q polybar
 
-polybar 2>&1 | tee -a /tmp/polybar.log & disown
-echo "Polybar launched..."
+# launch polybar on all monitors
+if type "xrandr" > /dev/null 2>&1; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload 2>&1 | tee -a /tmp/polybar_"$m".log & disown
+	done
+else
+	# single monitor fallback if can't find xrandr or something
+	polybar 2>&1 | tee -a /tmp/polybar.log & disown
+fi
+
+echo "Polybar(s) launched..."
