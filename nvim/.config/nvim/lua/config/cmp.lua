e7118966ea95635e3558bbd9c29efa7841821936
Hod Kimhi <hodkimhi@gmail.com>
2025-09-02 01:50:43 -0700

    major overhaul of nvim-cmp config - added mappings, snippet support with luasnip, various sources, and some formatting

--- nvim/.config/nvim/lua/config/cmp.lua
+++ nvim/.config/nvim/lua/config/cmp.lua
@@ -1,42 +1,108 @@
-require("cmp").setup({
+-- extremely helpful site for understanding WTF cmp configuration is:
+-- https://vonheikemen.github.io/devlog/tools/setup-nvim-lspconfig-plus-nvim-cmp/
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
 	snippet = {
 		expand = function(args)
-			vim.fn["UltiSnips#Anon"](args.body)
+			-- vim.snippet.expand(args.body)
+			luasnip.lsp_expand(args.body)
 		end,
 	},
+	mapping = {
+		["<CR>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				if luasnip.expandable() then
+					luasnip.expand()
+				else
+					cmp.confirm({
+						select = true,
+					})
+				end
+			else
+				fallback()
+			end
+		end),
+
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.locally_jumpable(1) then
+				luasnip.jump(1)
+			else
+				fallback()
+			end
+		end, { "i", "s", "c" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s", "c" }),
+
+		--[[ ["<Tab>"] = cmp.mapping(function(fallback)
+			-- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+			if cmp.visible() then
+				local entry = cmp.get_selected_entry()
+				if not entry then
+					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+				end
+				cmp.confirm()
+			else
+				fallback()
+			end
+		end, { "i", "s", "c" }), ]]
+
+		["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+		["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+		["<C-e>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
+		["<C-y>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "spell" },
+	},
+	window = {
+		documenation = cmp.config.window.bordered(),
+	},
 	formatting = {
-		format = function(entry, vim_item)
-			vim_item.menu = ({
+		fields = { "menu", "abbr", "kind" },
+		format = function(entry, item)
+			local menu_icon = {
 				nvim_lsp = "[LSP]",
 				spell = "[Spellings]",
-				zsh = "[Zsh]",
 				buffer = "[Buffer]",
-				ultisnips = "[Snip]",
+				luasnip = "[Snip]",
 				treesitter = "[Treesitter]",
 				calc = "[Calculator]",
 				nvim_lua = "[Lua]",
 				path = "[Path]",
 				nvim_lsp_signature_help = "[Signature]",
 				cmdline = "[Vim Command]",
-			})[entry.source.name]
-			return vim_item
+			}
+
+			item.menu = menu_icon[entry.source.name]
+			return item
 		end,
 	},
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "nvim_lsp_signature_help" },
-		{
-			name = "spell",
-			option = {
-				keep_all_entries = false,
-				enable_in_context = function()
-					return true
-				end,
-			},
-		},
-		{ name = "path" },
+})
+
+cmp.setup.cmdline("/", {
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp_document_symbol" },
+	}, {
 		{ name = "buffer" },
-	},
+	}),
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
