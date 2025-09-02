1ad58505bcacf72b3b669e841126aa99f672a9c8
Hod Kimhi <hodkimhi@gmail.com>
2025-09-02 14:07:29 -0700

    re-enable lazy loading for telescope

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -43,7 +43,6 @@ require("lazy").setup({
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
-		lazy = false,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope-file-browser.nvim",
